34899124f325546c3f8c5d85afebc7e7d101945f
JosephineJoseph812 <141255046+JosephineJoseph812@users.noreply.github.com>
2025-04-23 00:43:02 +0200

    test

--- DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
+++ DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7578c027b52db44433b2fc05f4e79ebe3c5f778")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2a0ac7e921427153df2df0e2b5c72f349440c82")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
