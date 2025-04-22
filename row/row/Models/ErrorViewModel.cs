bfe0da5ff8dcd3aa6676f94919a50eb8d89a4554
Abanoub1009 <118397149+Abanoub1009@users.noreply.github.com>
2025-04-22 23:58:55 +0200

    Delete row directory

--- row/row/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace row.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
