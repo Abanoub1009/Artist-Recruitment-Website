d4e1028d8b933c58fa144fc4e647552387b9e89e
Abanoub1009 <118397149+Abanoub1009@users.noreply.github.com>
2025-04-23 00:16:07 +0200

    delete

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Artist_Recruitment_Website.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
