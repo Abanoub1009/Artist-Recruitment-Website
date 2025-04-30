436cc879271b7c057fd42bae9b54aa98d370cb29
Abanoub1009 <118397149+Abanoub1009@users.noreply.github.com>
2025-05-01 01:49:20 +0300

    test

--- Models/obj/Debug/net8.0/Models.AssemblyInfo.cs
+++ Models/obj/Debug/net8.0/Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d36dcfd7cdb23c14eeafddee5581d305dd1797e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20eb6d362e62ef97979dce7afe6c37ebdb9e71e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
